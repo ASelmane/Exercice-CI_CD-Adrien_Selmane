3196f5967e65ff8b767d99bc895e58cd8f422f36
ASelmane <adrien.selmane@gmail.com>
2025-01-29 11:45:32 +0100

    changement test

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ import { sum } from "./index";
 
 describe("sum", () => {
   it("should fail when adding two numbers incorrectly", () => {
-    expect(sum(1, 2)).toBe(4); // This test will fail
+    expect(sum(1, 2)).not.toBe(4); // This test will pass
   });
 
   it("should add two numbers", () => {
